ee91acc5b9c71e682e2abbf4258dd4f2a409c63d
SamuelKrakheckeDias <161588543+SamuelKrakheckeDias@users.noreply.github.com>
2024-08-20 08:41:12 -0300

    adicionei mais codigos no js

--- js/script.js
+++ js/script.js
@@ -4,15 +4,10 @@ function adicionarValor() {
 
   const valor = Number(document.getElementById('valor').value);
 
-
-
   if (!isNaN(valor)) {
     vetor.push(valor);
 
-
-
     const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
-
 
     const novaLinha = tabela.insertRow();
     const celula = novaLinha.insertCell(0);
@@ -22,4 +17,28 @@ function adicionarValor() {
     document.getElementById('valor').focus();
 
   }
+}
+
+function mostrarPares() {
+  console.log(' entrando na função mostrarPares')
+  const tabelaPares = document.getElementById('tabelaPares').getElementsByTagName('tbody')[0];
+
+
+    console.log('limpando a tabela')
+    tabelaPares.innerHTML = ''; // Limpa a tabela antes de adicionar novos Valores
+
+
+    vetor.forEach(valor => {
+     console.log('dentro do for')
+
+      if (valor % 2 === 0) {
+        console.log('dentro do if')
+
+         const novaLinha = tabelaPares.insertRow();
+         const celula = novaLinha.insertCell(0);
+         celula.textContent = valor;
+
+      }
+   });
+
 }
