aa4ea0222f15e90ad895d08c33255b8f18a20bea
SamuelKrakheckeDias <161588543+SamuelKrakheckeDias@users.noreply.github.com>
2024-08-27 08:07:25 -0300

    arrumei o js e o index

--- js/script.js
+++ js/script.js
@@ -1,44 +1,30 @@
 let vetor = [];
 
 function adicionarValor() {
-
   const valor = Number(document.getElementById('valor').value);
+  vetor.push(valor);
 
-  if (!isNaN(valor)) {
-    vetor.push(valor);
+  const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
+  const novaLinha = tabela.insertRow();
+  const celula = novaLinha.insertCell(0);
+  celula.textContent = valor;
 
-    const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
-
-    const novaLinha = tabela.insertRow();
-    const celula = novaLinha.insertCell(0);
-    celula.textContent = valor;
-
-    document.getElementById('valor').value = '';
-    document.getElementById('valor').focus();
-
-  }
+  document.getElementById('valor').value = '';
+  document.getElementById('valor').fcous()
 }
 
 function mostrarPares() {
-  console.log(' entrando na função mostrarPares')
-  const tabelaPares = document.getElementById('tabelaPares').getElementsByTagName('tbody')[0];
+  const tabela = document.getElementById("tabelaPares").getElementsByTagName("tbody")[0];
 
+  // Limpa a tabela antes de adicionar novos valores
+  tabela.innerHTML = '';
 
-    console.log('limpando a tabela')
-    tabelaPares.innerHTML = ''; // Limpa a tabela antes de adicionar novos Valores
-
-
-    vetor.forEach(valor => {
-     console.log('dentro do for')
-
-      if (valor % 2 === 0) {
-        console.log('dentro do if')
-
-         const novaLinha = tabelaPares.insertRow();
-         const celula = novaLinha.insertCell(0);
-         celula.textContent = valor;
-
-      }
-   });
-
+  for (let i = 0; i < vetor.length; i++) {
+    const valor = vetor[i];
+    if (valor % 2 === 0) {
+      const linha = tabela.insertRow();
+      const celula = linha.insertCell();
+      celula.textContent = valor;
+    }
+  }
 }
